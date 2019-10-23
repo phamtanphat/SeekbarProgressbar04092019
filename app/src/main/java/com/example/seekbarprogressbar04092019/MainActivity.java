de76e3a4a5ac9d4be2ad3edb4c5c767f3027f7ca
phatdroid94 <phatdroid94@gmail.com>
2019-10-23 20:38:21 +0700

    finish seekbar

--- app/src/main/java/com/example/seekbarprogressbar04092019/MainActivity.java
+++ app/src/main/java/com/example/seekbarprogressbar04092019/MainActivity.java
@@ -8,19 +8,24 @@ import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    SeekBar sk;
+    SeekBar sk,sk1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         sk = findViewById(R.id.seekbar);
+        sk1 = findViewById(R.id.seekbar1);
 
-        sk.setMax(20);
-        sk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        sk.setIndeterminate(true);
+        sk1.setIndeterminate(true);
+        sk1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.d("BBB",progress + "");
+//                if (progress > sk.getProgress()){
+//                    sk1.setProgress(sk.getProgress());
+//                }
             }
 
             @Override
