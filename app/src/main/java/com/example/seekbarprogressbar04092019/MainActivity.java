f6d346d10a3dcdd7688707699c0dce80db066863
phatdroid94 <phatdroid94@gmail.com>
2019-10-23 21:02:57 +0700

    source image

--- app/src/main/java/com/example/seekbarprogressbar04092019/MainActivity.java
+++ app/src/main/java/com/example/seekbarprogressbar04092019/MainActivity.java
@@ -4,39 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    SeekBar sk,sk1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sk = findViewById(R.id.seekbar);
-        sk1 = findViewById(R.id.seekbar1);
 
-        sk.setIndeterminate(true);
-        sk1.setIndeterminate(true);
-        sk1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-//                if (progress > sk.getProgress()){
-//                    sk1.setProgress(sk.getProgress());
-//                }
-            }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
     }
 }
