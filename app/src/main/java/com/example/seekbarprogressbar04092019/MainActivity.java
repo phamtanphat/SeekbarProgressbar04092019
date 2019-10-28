990aa209f9a56f7021bed0720df284645b94d245
phatdroid94 <phatdroid94@gmail.com>
2019-10-28 20:58:32 +0700

    feat<Start game> : handle single check and must choose one checkbox

--- app/src/main/java/com/example/seekbarprogressbar04092019/MainActivity.java
+++ app/src/main/java/com/example/seekbarprogressbar04092019/MainActivity.java
@@ -4,18 +4,79 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ImageButton imgStart;
+    CheckBox cbOne,cbTwo,cbThree;
+    SeekBar skOne,skTwo,skThree;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        singleCheckedCheckbox();
+        eventClick();
+    }
+
+    private void singleCheckedCheckbox() {
+        cbOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    cbTwo.setChecked(false);
+                    cbThree.setChecked(false);
+                }
+            }
+        });
+        cbTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    cbOne.setChecked(false);
+                    cbThree.setChecked(false);
+                }
+            }
+        });
+        cbThree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    cbOne.setChecked(false);
+                    cbTwo.setChecked(false);
+                }
+            }
+        });
+    }
+
+    private void eventClick() {
+        imgStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cbOne.isChecked() || cbTwo.isChecked() || cbThree.isChecked()){
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Hãy đặt cược!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
 
-
+    private void init() {
+        imgStart = findViewById(R.id.imagebuttonStart);
+        cbOne = findViewById(R.id.checkboxOne);
+        cbTwo = findViewById(R.id.checkboxTwo);
+        cbThree = findViewById(R.id.checkboxThree);
+        skOne = findViewById(R.id.seekbarOne);
+        skTwo = findViewById(R.id.seekbarTwo);
+        skThree = findViewById(R.id.seekbarThree);
     }
 }
